e5cd78956447176b703a4b59109120e468d68c8b
Nat Goodspeed <nat@lindenlab.com>
2023-03-20 12:02:11 -0400

    SL-18837: Explicitly call FormatMessageA() for char* arguments.
    The FormatMessage() alias was expanding to FormatMessageW(), producing
    warnings about passing char* arguments to wchar_t* parameters -- which would
    have gone badly.

--- curl/tests/server/util.c
+++ curl/tests/server/util.c
@@ -148,8 +148,8 @@ void win32_perror(const char *msg)
   char buf[512];
   DWORD err = SOCKERRNO;
 
-  if(!FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, err,
-                    LANG_NEUTRAL, buf, sizeof(buf), NULL))
+  if(!FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, err,
+                     LANG_NEUTRAL, buf, sizeof(buf), NULL))
      snprintf(buf, sizeof(buf), "Unknown error %lu (%#lx)", err, err);
   if(msg)
     fprintf(stderr, "%s: ", msg);
